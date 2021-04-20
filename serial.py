42f75471201a696529097863f79868cb08857782
SAYASENE <MITCHELLSAYASENE@GMAIL.COM>
2021-04-20 19:18:33 -0400

    passed all tests for serial.py

--- serial.py
+++ serial.py
@@ -17,3 +17,21 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start):
+        """set starting serial number"""
+        self.start = start-1
+        self.counter = 0
+
+    def __repr__(self):
+        return f"<SerialGenerator, starting serial number = {self.start}>"
+
+    def generate(self):
+        """increment starting serial number by 1 on each call"""
+        self.counter += 1
+        return self.start + self.counter
+
+    def reset(self):
+        """reset the serial number to the starting value"""
+        self.counter = 0
+        
