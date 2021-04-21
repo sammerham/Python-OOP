2a0579e79077ea518cbdc14d39b4dad6bfaeef76
Sam Merham <samehisaac@yahoo.com>
2021-04-20 18:00:25 -0700

    completed step2)

--- wordfinder.py
+++ wordfinder.py
@@ -3,10 +3,17 @@ class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self, path):
         file = open(path)
-        txt = file.read()
-        self.word_list = txt.split()
+        self.word_list = self.split_words(file)
         print(f"{len(self.word_list)} words read")
     
+    def split_words(self, txt):
+        return [word.strip() for word in txt]
+
     def random(self):
         return choice(self.word_list)
-    
+
+
+class SpecialWordFinder(WordFinder):
+    def split_words(self, txt):
+        return [word.strip() for word in txt if word != "" and not word.startswith('#')]
+        
