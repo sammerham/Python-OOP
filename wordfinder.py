8aadd9463fea13f7bbcfad496559a3c9f8588fc3
Sam Merham <samehisaac@yahoo.com>
2021-04-20 17:09:39 -0700

    first part of random words

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,12 @@
+from random import choice 
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, path):
+        file = open(path)
+        txt = file.read()
+        self.word_list = txt.split()
+        print(f"{len(self.word_list)} words read")
+    
+    def random(self):
+        return choice(self.word_list)
+    
