b9f3e09b59a990f0be13353597f6c9ee75ca2d30
Sam Merham <samehisaac@yahoo.com>
2021-04-20 18:07:45 -0700

    added doc strings

--- wordfinder.py
+++ wordfinder.py
@@ -1,4 +1,5 @@
 from random import choice 
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self, path):
@@ -6,14 +7,19 @@ class WordFinder:
         self.word_list = self.split_words(file)
         print(f"{len(self.word_list)} words read")
     
-    def split_words(self, txt):
-        return [word.strip() for word in txt]
-
+    def split_words(self, file):
+        """Remove white space and return words in a list."""
+        return [word.strip() for word in file]
+    
     def random(self):
+        """Return Random word from a list"""
         return choice(self.word_list)
 
 
 class SpecialWordFinder(WordFinder):
     def split_words(self, txt):
-        return [word.strip() for word in txt if word != "" and not word.startswith('#')]
+        """Remove white space and return words in a list if word isn't white space or starts with #."""
+        return [word.strip() 
+            for word in txt 
+            if word != "" and not word.startswith('#')]
         
